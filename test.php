8fb3350faa14c6c933721d19e0a0e7751d6a448e
Muhammad Rifqi <muhammad45rifki@gmail.com>
2025-11-06 09:55:30 +0700

    update

--- test.php
+++ test.php
@@ -1,7 +1,14 @@
 <?php
-// Jalankan script Python
 // $output = shell_exec("run_training.py 2>&1");
 $output = shell_exec('python project-prediction-app-main/run_training.py');
-// Tampilkan hasil di browser
-echo "<pre>$output</pre>";
+$input = file_put_contents('log.txt', $output);
+if($input){
+    $jsonData = file_get_contents('http://localhost/project-prediction-app-main/model/training_results.json');
+    $encode = json_decode($jsonData,true);
+    $array = array(
+        "success" => true,
+    );
+}
+
+
 ?>
